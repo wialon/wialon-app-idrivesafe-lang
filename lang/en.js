1addd3794e4a3e6f16ec3553aab20299b0781702
Aleksey Shmigelski <shmi@gurtam.com>
2014-03-18 15:41:40 +0300

    en-ru languages added

---
+++ lang/en.js
@@ -0,0 +1,66 @@
+TRANSLATIONS = {
+	"Today": "",
+	"Yesterday": "",
+	"Week": "",
+	"Month": "",
+	"Custom": "",
+	"Previous": "",
+	"Next": "",
+	
+	"Login error": "",
+	"Error": "",
+	"Error while getting data": "",
+	"No data for selected interval": "",
+	"Limit of tabs reached": "",
+	"Error getting settings from server": "",
+	
+	"Sort by": "",
+	"name": "",
+	
+	"Settings": "",
+	"settings": "",
+	
+	"Unit": "",
+	"Add unit": "",
+	
+	"Mileage": "",
+	"mileage": "",
+	"km": "",
+	"km/h": "",
+	"mi": "",
+	"mph": "",
+
+	"violations / km": "",
+	"( %s km/h for %s s )": "(<b>%s</b> km/h for <b>%s</b> s)",
+	"( %s mph for %s s )": "(<b>%s</b> mph for <b>%s</b> s)",
+	"Duration": "",
+	
+	"Rate": "",
+	
+	"Violations": "",
+	"violations": "",
+	"Violations of the trip": "",
+	"No violations": "",
+	"Trips for %s ": "",
+	"not shorter than %s": "",
+	
+	"trips count": "",
+	"Trip length": "",
+	"Minimal trip length": "",
+	
+	"Acceleration": "",
+	"Brake": "",
+	"Turn": "",
+	"Unknown": "",
+	
+	"New tab": "",
+	"Close tab": "",
+	
+	"Delete statistics": "",
+	
+	"Show on map": "",
+	
+	"Loyal assessment": "",
+	"Normal assessment": "",
+	"Strict assessment": ""
+};
---
+++ lang/ru.js
@@ -0,0 +1,66 @@
+TRANSLATIONS = {
+	"Today": "Сегодня",
+	"Yesterday": "Вчера",
+	"Week": "Неделя",
+	"Month": "Месяц",
+	"Custom": "Другой",
+	"Previous": "Назад",
+	"Next": "Вперед",
+	
+	"Login error": "Ошибка авторизации",
+	"Error": "Ошибка",
+	"Error while getting data": "Ошибка получения данных",
+	"No data for selected interval": "Нет данных за выбранный период",
+	"Limit of tabs reached": "Открыто максимальное количество вкладок",
+	"Error getting settings from server": "Ошибка получения настроек с сервера",
+	
+	"Sort by": "Сортировать по",
+	"name": "имени",
+	
+	"Settings": "Настройки",
+	"settings": "настройкам",
+	
+	"Unit": "Объект",
+	"Add unit": "Добавить объект",
+	
+	"Mileage": "Пробег",
+	"mileage": "пробегу",
+	"km": "км",
+	"mi": "ми",
+	"km/h": "км/ч",
+	"mph": "ми/ч",
+
+	"violations / km": "нарушений / км",
+	"( %s km/h for %s s )": "(<b>%s</b> км/ч за <b>%s</b> с)",
+	"( %s mph for %s s )": "(<b>%s</b> ми/ч за <b>%s</b> с)",
+	"Duration": "Длительность",
+	
+	"Rate": "Оценка",
+	
+	"Violations": "Нарушения",
+	"violations": "нарушениям",
+	"Violations of the trip": "Нарушения за поездку",
+	"No violations": "Нет нарушений",
+	"Trips for %s ": "Поездки за %s ",
+	"not shorter than %s": "не короче %s",
+	
+	"trips count": "количеству поездок",
+	"Trip length": "Поездка",
+	"Minimal trip length": "Минимальная длина поездки",
+	
+	"Acceleration": "Ускорение",
+	"Brake": "Торможение",
+	"Turn": "Поворот",
+	"Unknown": "Неизвестно",
+	
+	"New tab": "Новая вкладка",
+	"Close tab": "Закрыть вкладку",
+	
+	"Delete statistics": "Удалить статистику",
+	
+	"Show on map": "Показать на карте",
+	
+	"Loyal assessment": "Лояльная оценка",
+	"Normal assessment": "Нормальная оценка",
+	"Strict assessment": "Строгая оценка"
+};
