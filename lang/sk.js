80b1ecd570bdfe91d6e32c831538c1028c3bc7f6
Mimicar <gps@gpsportal.sk>
2014-05-30 07:30:35 +0200

    Create sk.js

---
+++ lang/sk.js
@@ -0,0 +1,66 @@
+TRANSLATIONS = {
+	"Today": "Dnes",
+	"Yesterday": "Včera",
+	"Week": "Týždeň",
+	"Month": "Mesiac",
+	"Custom": "Dátum",
+	"Previous": "Predchádzajúci",
+	"Next": "Ďalší",
+
+	"Login error": "Chyba prihlásenia",
+	"Error": "Chyba",
+	"Error while getting data": "Chyba pri získavaní dát",
+	"No data for selected interval": "Žiadne dáta pre zvolený interval",
+	"Limit of tabs reached": "Bol dosiahnutý limit záložiek",
+	"Error getting settings from server": "Chyba pri načítaní nastavení zo servera",
+
+	"Sort by": "Zoradiť podľa",
+	"name": "názov",
+
+	"Settings": "Nastavenia",
+	"settings": "nastavenia",
+
+	"Unit": "Vozidlo",
+	"Add unit": "Pridať vozidlo",
+
+	"Mileage": "Vzdialenosť",
+	"mileage": "vzdialenosť",
+	"km": "km",
+	"km/h": "km/h",
+	"mi": "",
+	"mph": "",
+
+	"violations / km": "priestupky / km",
+	"( %s km/h for %s s )": "(<b>%s</b> km/h for <b>%s</b> s)",
+	"( %s mph for %s s )": "(<b>%s</b> mph for <b>%s</b> s)",
+	"Duration": "Trvanie",
+
+	"Rate": "Hodnotenie",
+
+	"Violations": "Priestupky",
+	"violations": "priestupky,
+	"Violations of the trip": "Priestupky na úseku trasy",
+	"No violations": "Bez priestupkov",
+	"Trips for %s ": "Jazdy z %s",
+	"not shorter than %s": "nie kratšie ako %s",
+
+	"trips count": "Počet trás",
+	"Trip length": "Dĺžka trasy",
+	"Minimal trip length": "Minimálna dĺžka trasy",
+
+	"Acceleration": "Zrýchlenie",
+	"Brake": "Brzdenie",
+	"Turn": "Prejazd zákrutou",
+	"Unknown": "Neznámy",
+
+	"New tab": "Nová záložka",
+	"Close tab": "Zavrieť záložku",
+
+	"Delete statistics": "Vymazať štatistiku",
+
+	"Show on map": "Zobraziť na mape",
+
+	"Loyal assessment": "Lojálne hodnotenie",
+	"Normal assessment": "Normálne hodnotenie",
+	"Strict assessment": "Prísne hodnotenie"
+};
