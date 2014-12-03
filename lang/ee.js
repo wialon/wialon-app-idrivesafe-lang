0c3ddd95ec87c7f449a15886452cfb1d93ba52d3
GoGPS <eduard.vald@gogps.eu>
2014-12-03 10:27:29 +0200

    Create ee.js

---
+++ lang/ee.js
@@ -0,0 +1,66 @@
+TRANSLATIONS = {
+"Today": "Täna",
+	"Yesterday": "Eile",
+	"Week": "Nädal",
+	"Month": "Kuu",
+	"Custom": "Muu",
+	"Previous": "Eelmine",
+	"Next": "Järgmine",
+	
+	"Login error": "Viga sisselogimisel",
+	"Error": "Viga",
+	"Error while getting data": "Viga andmete vastuvõtmisel",
+	"No data for selected interval": "Valitud ajalõigu kohta puuduvad andmed",
+	"Limit of tabs reached": "Tabelite piirarv on ületatud",
+	"Error getting settings from server": "Viga serveriseadete vastuvõtmisel",
+	
+	"Sort by": "Sorteerimisalused",
+	"name": "nime alusel",
+	
+	"Settings": "Seaded",
+	"settings": "seadistustele",
+	
+	"Unit": "Objekt",
+	"Add unit": "Lisa objekt",
+	
+	"Mileage": "Läbisõit",
+	"mileage": "läbisõidule",
+	"km": "km",
+	"mi": "mi",
+	"km/h": "km/h",
+	"mph": "mi/h",
+
+	"violations / km": "rikkumisi/km",
+	"( %s km/h for %s s )": "(<b>%s</b> km/h <b>%s</b> с  kestel)",
+	"( %s mph for %s s )": "(<b>%s</b mi/h <b>%s</b> с kestel)",
+	"Duration": "Kestus",
+	
+	"Rate": "Hinne",
+	
+	"Violations": "Rikkumised",
+	"violations": "rikkumistele",
+	"Violations of the trip": "rikkumisi teekonna kohta",
+	"No violations": "Rikkumisi pole",
+	"Trips for %s ": "Sõite %s kohta",
+	"not shorter than %s": "mitte lühem kui %s",
+	
+	"trips count": "Teekondade arvule",
+	"Trip length": "Teekond",
+	"Minimal trip length": "Teekonna minimaalne pikkus",
+	
+	"Acceleration": "Kiirendamine",
+	"Brake": "Pidurdamine",
+	"Turn": "Pööre",
+	"Unknown": "Teadmata",
+	
+	"New tab": "Uus tabel",
+	"Close tab": "Sulge tabel",
+	
+	"Delete statistics": "Kustuta statistika",
+	
+	"Show on map": "Näita kaardil",
+	
+	"Loyal assessment": "Leebe hindamine",
+	"Normal assessment": "Normaalne hindamine",
+	"Strict assessment": "Range hindamine"
+};
