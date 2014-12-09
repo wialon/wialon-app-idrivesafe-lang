5f7b54b81fb02feb7443631cebe50d14557d631e
GoGPS <eduard.vald@gogps.eu>
2014-12-09 20:49:44 +0200

    Create lv.js

---
+++ lang/lv.js
@@ -0,0 +1,66 @@
+TRANSLATIONS = {
+	"Today": "Šodien",
+	"Yesterday": "Vakar",
+	"Week": "Nedeļa",
+	"Month": "Mēnesis",
+	"Custom": "Cits",
+	"Previous": "Atpakāļ",
+	"Next": "Uzpriekšu",
+	
+	"Login error": "Autorizācijas kļūda",
+	"Error": "Kļūda",
+	"Error while getting data": "Datu saņemšanas kļūda",
+	"No data for selected interval": "Nav datu par izvēlēto periodu",
+	"Limit of tabs reached": "Ir atvērts maksimāls ieliktņu skaits",
+	"Error getting settings from server": "Uzstādījumu saņemšanas kļūda no servera",
+	
+	"Sort by": "Sortēt",
+	"name": "pēc vārda",
+	
+	"Settings": "Izvēlnes",
+	"settings": "pēc izvēlnēm",
+	
+	"Unit": "Objekts",
+	"Add unit": "Pievienot objektu",
+	
+	"Mileage": "Nobraukums",
+	"mileage": "pēc nobraukuma",
+	"km": "km",
+	"mi": "mi",
+	"km/h": "km/st",
+	"mph": "mi/s",
+
+	"violations / km": "pārkāpumu/ km",
+	"( %s km/h for %s s )": "(<b>%s</b> кm/s par <b>%s</b> с)",
+	"( %s mph for %s s )": "(<b>%s</b> mi/s par <b>%s</b> с)",
+	"Duration": "Ilgums",
+	
+	"Rate": "Novērtējums",
+	
+	"Violations": "Pārkāpums",
+	"violations": "pārkāpumiem",
+	"Violations of the trip": "Pārkapumi par braucienu",
+	"No violations": "Pārkāpumu nav",
+	"Trips for %s ": "Braucieni par %s ",
+	"not shorter than %s": "ne īsāk %s",
+	
+	"trips count": "braucienu skaitu",
+	"Trip length": "Brauciens",
+	"Minimal trip length": "Brauciena minimāls gārums",
+	
+	"Acceleration": "Paātrināšana",
+	"Brake": "Bremzēšana",
+	"Turn": "Pagrieziens",
+	"Unknown": "Nav zināms",
+	
+	"New tab": "Jauns ieliktnis",
+	"Close tab": "Aizvērt ieliktni",
+	
+	"Delete statistics": "Nodzēst statistiku",
+	
+	"Show on map": "Pārādīt māpē",
+	
+	"Loyal assessment": "Lojāls novērtējums",
+	"Normal assessment": "Normāls novērtējums",
+	"Strict assessment": "Stingrs novērtējums"
+};
