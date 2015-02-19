0e6aeb660acef49ade94ee1bcbfb00a5840ce480
eflotta <eflotta@users.noreply.github.com>
2015-02-19 12:33:38 +0100

    Create hu.js
    Hello, Please add this hungarian version to the iDrivesafe App.
    Regards, Viktor

---
+++ lang/hu.js
@@ -0,0 +1,66 @@
+TRANSLATIONS = {
+	"Today": "Ma",
+	"Yesterday": "Tegnap",
+	"Week": "Hét",
+	"Month": "Hónap",
+	"Custom": "Időszak",
+	"Previous": "Előző",
+	"Next": "Következő",
+	
+	"Login error": "Sikertelen belépés.",
+	"Error": "Ismeretlen hiba.",
+	"Error while getting data": "Hiba az adatok lekérése során.",
+	"No data for selected interval": "Nincsenek adatok a kért időszakra.",
+	"Limit of tabs reached": "Elérte a lapok maximális számát.",
+	"Error getting settings from server": "Szerver nem elérhető.",
+	
+	"Sort by": "Rendezés",
+	"name": "név",
+	
+	"Settings": "Beállítások",
+	"settings": "beállítások",
+	
+	"Unit": "Jármű",
+	"Add unit": "Jármű hozzáadása",
+	
+	"Mileage": "Távolság",
+	"mileage": "távolság",
+	"km": "",
+	"km/h": "",
+	"mi": "",
+	"mph": "",
+
+	"Figyelmeztetés / km": "",
+	"( %s km/h for %s s )": "(<b>%s</b> km/h -val <b>%s</b> mp-ig)",
+	"( %s mph for %s s )": "(<b>%s</b> mph for <b>%s</b> s)",
+	"Duration": "Menetidő",
+	
+	"Rate": "Értékelés",
+	
+	"Violations": "Figyelmeztetések",
+	"violations": "figyelmeztetések",
+	"Violations of the trip": "Figyelmeztetések",
+	"No violations": "Nincsek figyelmeztetések ",
+	"Trips for %s ": "Vizsgált útszakaszok:",
+	"not shorter than %s": "minimális táv:",
+	
+	"trips count": "útszakaszok",
+	"Trip length": "Útszakasz adatai:",
+	"Minimal trip length": "Minimális útszakasz",
+	
+	"Acceleration": "Gyorsítás",
+	"Brake": "Fékezés",
+	"Turn": "Kanyarodás",
+	"Unknown": "Egyéb",
+	
+	"New tab": "Új lap",
+	"Close tab": "Bezárás",
+	
+	"Delete statistics": "Sztatisztika törlése",
+	
+	"Show on map": "Megjelenítés térképen",
+	
+	"Loyal assessment": "Enyhe pontozás",
+	"Normal assessment": "Normál pontozás",
+	"Strict assessment": "Szigorú pontozás"
+};
